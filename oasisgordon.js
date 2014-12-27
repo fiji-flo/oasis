b9cd8e28f4009f502776f81c4047e871c6db79f7
fiji-flo <flome.rz@gmail.com>
2014-12-28 00:11:52 +0100

    add first output file support.

--- oasisgordon.js
+++ oasisgordon.js
@@ -2,7 +2,11 @@ var fs = require('fs');
 var RESOLUTION = 1000;
 
 process.on('message', function (m) {
-    fm.start();
+    if (fm.open) {
+        fm.stop();
+    } else {
+        fm.start();
+    }
     console.log("gordon got: " + m.text);
 });
 
@@ -11,29 +15,36 @@ process.on('SIGINT', function () {
     console.log("gordon out!");
 });
 
-
 function readSensors() {
     return [1,2,3,4];
 }
 
 function FileManager() {
     this.timer = null;
+    this.fileName = "/tmp/log1.csv";
+    this.open = false;
 }
 
 FileManager.prototype.start = function () {
     console.log("starting timer");
     var self = this;
-    this.timer = setInterval(self.update, RESOLUTION);
+    this.ws = fs.createWriteStream(this.fileName, {
+        flags: 'a',
+        encoding: "utf-8"});
+    this.open = true;
+    this.timer = setInterval(function () { self.update(); }, RESOLUTION);
 };
 
 FileManager.prototype.stop = function () {
     console.log("stopping timer");
     clearInterval(this.timer);
+    this.ws.end();
 }
 
 FileManager.prototype.update = function () {
     var date = (new Date()).getTime();
-    console.log(date + " " + readSensors);
+    var line = date.toString() + ";" + readSensors().join(';') + '\n';
+    this.ws.write(line);
 };
 
 var fm = new FileManager();
