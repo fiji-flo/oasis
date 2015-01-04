f29c4dc879840aca49d2bccfdcba2f107cba1ec4
fiji-flo <flome.rz@gmail.com>
2015-01-04 22:50:08 +0100

    support for real sensors.

--- oasisgordon.js
+++ oasisgordon.js
@@ -1,12 +1,15 @@
 var fs = require('fs');
 var RESOLUTION = 1000;
+var FILELOCATION = '/home/oasis/logs/';
+var SENSORPREFIX = '/sys/bus/w1/devices/';
+var SENSORS = [
+    '28-000005fda813',
+    '28-000005fe6622'
+];
+var SENORFILENAME = 'w1_slave';
+var SENSORREGEX = /.*\n.*t=(\d+).*/;
 
 process.on('message', function (m) {
-    if (fm.open) {
-        fm.stop();
-    } else {
-        fm.start();
-    }
     console.log("gordon got: " + m.text);
 });
 
@@ -15,36 +18,98 @@ process.on('SIGINT', function () {
     console.log("gordon out!");
 });
 
+function pad(number) {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
+
+function readSensor(sensorName) {
+    var fileName = SENSORPREFIX + sensorName + '/' + SENORFILENAME;
+    var temp = NaN;
+    try {
+        var s = fs.readFileSync(fileName);
+        var groups = s.match(SENSORREGEX);
+        if (groups !== null) {
+            temp = int(groups[1]);
+        }
+    }
+    catch {
+        console.log("unable to read from " + fileName);
+    }
+    return temp;
+}
+
+
 function readSensors() {
-    return [1,2,3,4];
+    var temps = [];
+    for (var i in SENSORS) {
+        var temp = readSensor(SENSORS[i]);
+        temps.push(temp);
+    }
+    return temps;
 }
 
 function FileManager() {
     this.timer = null;
-    this.fileName = "/tmp/log1.csv";
-    this.open = false;
+    this.running = false;
+    this.currentDate = new Date();
+    this.ws = null;
+    this.openFile(this.currentDate);
+}
+
+FileManager.prototype.makeFileName = function (date) {
+    var dateString = date.getUTCFullYear() +
+        '-' + (date.getUTCMonth() + 1) +
+        '-' + date.getUTCDate() +
+        '-' + date.getUTCHours();
+        '-' + date.getUTCMinutes();
+    return FILELOCATION + dateString + '.csv';
 }
 
 FileManager.prototype.start = function () {
     console.log("starting timer");
     var self = this;
-    this.ws = fs.createWriteStream(this.fileName, {
-        flags: 'a',
-        encoding: "utf-8"});
-    this.open = true;
+    this.running = true;
     this.timer = setInterval(function () { self.update(); }, RESOLUTION);
 };
 
 FileManager.prototype.stop = function () {
     console.log("stopping timer");
     clearInterval(this.timer);
-    this.ws.end();
+}
+
+FileManager.prototype.openFile = function (date) {
+    var fileName = this.makeFileName(date);
+    this.ws = fs.createWriteStream(fileName, {
+        flags: 'a',
+        encoding: "utf-8"});
+};
+
+FileManager.prototype.closeFile = function () {
+    this.ws && this.ws.end();
+};
+
+FileManager.prototype.isNewDay = function (date) {
+    return this.currentDate.getUTCMinutes() !== date.getUTCMinutes();
+    //return this.currentDate.getUTCDate() !== date.getUTCDate();
+}
+
+FileManager.prototype.changeFile = function (date) {
+    if (this.isNewDay(date)) {
+        this.closeFile();
+        this.openFile(date);
+        this.currentDate = date;
+    };
 }
 
 FileManager.prototype.update = function () {
-    var date = (new Date()).getTime();
-    var line = date.toString() + ";" + readSensors().join(';') + '\n';
+    var date = new Date();
+    this.changeFile(date);
+    var line = date.getTime().toString() + ";" + readSensors().join(';') + '\n';
     this.ws.write(line);
 };
 
 var fm = new FileManager();
+fm.start();
